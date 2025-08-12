83ddd4ef23ade495331077ad7ebc649829938261
Erez Dagan <erezdagan5@gmail.com>
2025-08-12 12:22:42 +0300

    test: rely on package.json type=module; drop extensionsToTreatAsEsm

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   testEnvironment: "node",
-  // Treat .js files as ESM (your source and tests use `import`)
-  extensionsToTreatAsEsm: [".js"],
   collectCoverageFrom: ["src/**/*.js"],
-  // Keep reports for Jenkins
   reporters: ["default", ["jest-junit", { outputDirectory: "reports", outputName: "junit.xml" }]],
-  // No transforms needed; pure ESM JS
   transform: {}
 };
